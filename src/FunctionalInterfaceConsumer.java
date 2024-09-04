fdb370a18d9d57588f67c75e2ee0504fd601ee1c
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-04 09:53:22 -0300

    Adding JavaDoc

--- src/FunctionalInterfaceConsumer.java
+++ src/FunctionalInterfaceConsumer.java
@@ -2,9 +2,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * Class to make tests related to the functional interface consumer
+ */
 public class FunctionalInterfaceConsumer {
+    /**
+     * Numbers to be iterated
+     */
     static List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8);
 
+    /**
+     * Main method, to execute manual tests
+     * @param args CLI args (ignored)
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         FunctionalInterfaceConsumer.way1();
         System.out.println("-----------");
@@ -13,6 +24,9 @@ public class FunctionalInterfaceConsumer {
         FunctionalInterfaceConsumer.way3();
     }
 
+    /**
+     * One way to use the functional interface consumer
+     */
     private static void way1() {
         Consumer<Integer> imprimirNumeroPar = numero -> {
             if (numero % 2 == 0) {
@@ -23,6 +37,9 @@ public class FunctionalInterfaceConsumer {
         FunctionalInterfaceConsumer.numeros.forEach(imprimirNumeroPar);
     }
 
+    /**
+     * Another way to use the functional interface consumer
+     */
     private static void way2() {
         Consumer<Integer> imprimirNumeroPar = numero -> {
             if (numero % 2 == 0) {
@@ -34,6 +51,9 @@ public class FunctionalInterfaceConsumer {
         FunctionalInterfaceConsumer.numeros.stream().forEach(imprimirNumeroPar);
     }
 
+    /**
+     * Another way to use the functional interface consumer
+     */
     private static void way3() {
         FunctionalInterfaceConsumer.numeros.forEach(new Consumer<Integer>() {
             @Override
