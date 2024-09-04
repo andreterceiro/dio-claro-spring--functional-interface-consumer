edb875c49d9a3b11ea668418bfcf4ac9821291af
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-04 09:48:48 -0300

    Second way taught in class

--- src/FunctionalInterfaceConsumer.java
+++ src/FunctionalInterfaceConsumer.java
@@ -7,6 +7,8 @@ public class FunctionalInterfaceConsumer {
 
     public static void main(String[] args) throws Exception {
         FunctionalInterfaceConsumer.way1();
+        System.out.println("-----------");
+        FunctionalInterfaceConsumer.way2();
     }
 
     private static void way1() {
@@ -20,10 +22,16 @@ public class FunctionalInterfaceConsumer {
     }
 
     private static void way2() {
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
 
+        // see the stream() after numeros. Equals to way1 in all ohter parts
+        FunctionalInterfaceConsumer.numeros.stream().forEach(imprimirNumeroPar);
     }
 
     private static void way3() {
-
     }
 }
